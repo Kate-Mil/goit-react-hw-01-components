4c29557fd646d0c825fd18ccf4092be3ca1dc302
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-05-01 20:17:58 +0300

    4th task rendered

---
+++ src/components/TransactionHistory.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import TransactionRow from './TransactionRow';
+
+export default function TransactionHistory({ items }) {
+  return (
+    <table class="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <TransactionRow
+            key={id}
+            type={type}
+            amount={amount}
+            currency={currency}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
