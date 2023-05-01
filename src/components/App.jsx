55467f1355d8463593fd7e96a6f7773ab4f93dcb
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-05-01 20:14:15 +0300

    finished randering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import transactions from '../json/transactions.json';
 import Profile from './Profile';
 import Statistics from './Statistics';
 import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
 
 export const App = () => {
   return (
@@ -20,6 +21,8 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </>
   );
 };
