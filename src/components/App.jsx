ecbcfb22a8d7fc13ec081aa1997bd4636240b4db
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-05-02 01:50:29 +0300

    fixed commits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,19 +10,19 @@ import TransactionHistory from './TransactionHistory';
 export const App = () => {
   return (
     <>
-      {/* <Profile
+      <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      /> */}
-      {/* 
-      <Statistics title="Upload stats" stats={data} /> */}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />
 
-      {/* <TransactionHistory items={transactions} /> */}
+      <TransactionHistory items={transactions} />
     </>
   );
 };
