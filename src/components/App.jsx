2158ec3326f2bcc5371c5bbcebc3c3326f24d629
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-05-01 11:53:49 +0300

    fixed props according to HT

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,20 @@
-import Profile from "./Profile";
+import Profile from './Profile';
+import Statistics from './Statistics';
 import user from '../user.json';
+import data from '../data.json';
 
 export const App = () => {
- const {username,tag,location,avatar, stats:{follovers,views,likes}} = user;
   return (
-    <div
-      style={{
-        // height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-    <Profile
-    username = {username}
-    tag = {tag}
-    location = {location}
-    avatar = {avatar}
-    follovers = {follovers}
-    views = {views}
-    likes = {likes}
-    />
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics stats={data} />
     </div>
   );
 };
